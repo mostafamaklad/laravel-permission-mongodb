794c0093b1e0b062bd6d86cb9ff72cab232526a4
Mostafa Maklad <maklad@uxbert.com>
2017-10-05 16:44:57 +0300

    Assign permissions to role in commands

--- src/Commands/CreateRole.php
+++ src/Commands/CreateRole.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Maklad\Permission\Commands;
 
 use Illuminate\Console\Command;
+use Maklad\Permission\Contracts\PermissionInterface as Permission;
 use Maklad\Permission\Contracts\RoleInterface as Role;
 
 /**
@@ -14,19 +15,29 @@ class CreateRole extends Command
 {
     protected $signature = 'permission:create-role
         {name : The name of the role}
-        {guard? : The name of the guard}';
+        {guard? : The name of the guard}
+        {--permission=* : The name of the permission}';
 
     protected $description = 'Create a role';
 
     public function handle()
     {
-        $roleClass = \app(Role::class);
+        $roleClass       = \app(Role::class);
+        $permissionClass = \app(Permission::class);
+
+        $name        = $this->argument('name');
+        $guard       = $this->argument('guard');
+        $permissions = $this->option('permission');
 
         $role = $roleClass::create([
-            'name'       => $this->argument('name'),
-            'guard_name' => $this->argument('guard')
+            'name'       => $name,
+            'guard_name' => $guard
         ]);
 
         $this->info("Role `{$role->name}` created");
+
+        $role->givePermissionTo($permissions);
+        $permissions_str = $role->permissions->implode('name', '`, `');
+        $this->info("Permissions `{$permissions_str}` has been given to role `{$role->name}`");
     }
 }
