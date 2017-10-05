46e150448ba525944dd1b521105d407b3b84da4e
Mostafa Maklad <dev.mostafa.maklad@gmail.com>
2017-10-05 16:44:57 +0300

    Assign permissions to role in commands

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -3,8 +3,8 @@
 namespace Maklad\Permission\Test;
 
 use Artisan;
-use Maklad\Permission\Models\Role;
 use Maklad\Permission\Models\Permission;
+use Maklad\Permission\Models\Role;
 
 class CommandTest extends TestCase
 {
@@ -48,5 +48,40 @@ class CommandTest extends TestCase
         $this->assertCount(1, Permission::where('name', 'new-permission')
             ->where('guard_name', 'api')
             ->get());
+    }
+
+    /** @test */
+    public function it_can_create_a_role_and_assign_permissions()
+    {
+        Artisan::call('permission:create-permission', ['name' => 'test1',]);
+        Artisan::call('permission:create-permission', ['name' => 'test2',]);
+        Artisan::call('permission:create-role', [
+            'name'         => 'new-role',
+            '--permission' => ['test1', 'test2']
+        ]);
+
+        $role = Role::findByName('new-role');
+        $this->assertCount(2, $role->permissions);
+    }
+
+    /** @test */
+    public function it_can_create_a_role_with_guard_and_assign_permissions()
+    {
+        Artisan::call('permission:create-permission', [
+            'name'  => 'test1',
+            'guard' => 'api',
+        ]);
+        Artisan::call('permission:create-permission', [
+            'name'  => 'test2',
+            'guard' => 'api',
+        ]);
+        Artisan::call('permission:create-role', [
+            'name'         => 'new-role',
+            'guard'        => 'api',
+            '--permission' => ['test1', 'test2']
+        ]);
+
+        $role = Role::findByName('new-role', 'api');
+        $this->assertCount(2, $role->permissions);
     }
 }
